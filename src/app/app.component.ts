f387cc4c526352160ab04ea646e888119a75c45e
Jhonatan Plata <kaysinhoadsi@hotmail.com>
2021-06-14 15:57:09 -0500

    avances

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'elchaman';
+  constructor(){}
+
+  @HostListener('window:scroll', ['$event'])
+  scrolling($event){
+    let menu: any = document.getElementById("menu");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      menu.style.backgroundColor =  "white";
+      menu.style.color = "black";
+      menu.style.height = "105px";
+    } else {
+      menu.style.backgroundColor =  'transparent';
+      menu.style.height = "130px";    }
+  }
 }
